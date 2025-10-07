1d8ab05dd1f69b6d72567b02c25a52c27ce95456
Tomefy5 <tomefyandrytsiresy@gmail.com>
2025-10-07 15:53:55 +0200

    chore: setup shadcnUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1></h1>
+      <h1>Test</h1>
+      <Button className="text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white" variant={"outline"}>Test btn</Button>
     </div>
   );
 }
